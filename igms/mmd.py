6010b635db9a4b4d14d875ca4a2efdfb4e50d861
Dougal J. Sutherland <dougal@gmail.com>
2019-06-05 18:11:43 +0100

    corrected variance estimators in code

--- igms/mmd.py
+++ igms/mmd.py
@@ -74,7 +74,6 @@ def mmd2_and_variance(kernel_pair, estimator=Estimator.U_STAT):
             "Computing asymptotic variance for U-statistic estimator, "
             f"but using {estimator}."
         )
-    assert estimator == Estimator.U_STAT
     assert kernel_pair.n_X == kernel_pair.n_Y
     m = kernel_pair.n_X
     K = kernel_pair
@@ -87,32 +86,114 @@ def mmd2_and_variance(kernel_pair, estimator=Estimator.U_STAT):
     m1 = m - 1
     m1_m1 = m1 * m1
     m1_m1_m1 = m1_m1 * m1
+    m2 = m - 2
+    mdown2 = m * m1
+    mdown3 = mdown2 * m2
+    mdown4 = mdown3 * (m - 3)
+    twom3 = 2 * m - 3
 
-    # (5) of https://arxiv.org/abs/1611.04488v4
     var_est = (
-        (
-            (2 / (mm * m1_m1))
-            * (
-                2 * K.XX.offdiag_row_sums_sq_sum()
-                - K.XX.offdiag_sq_sum()
-                + 2 * K.YY.offdiag_row_sums_sq_sum()
-                - K.YY.offdiag_sq_sum()
-            )
+        (4 / mdown4) * (K.XX.offdiag_row_sums_sq_sum() + K.YY.offdiag_row_sums_sq_sum())
+        + (4 * (mm - m - 1) / (mmm * m1_m1))
+        * (K.XY.row_sums_sq_sum() + K.XY.col_sums_sq_sum())
+        - (8 / (mm * (mm - 3 * m + 2)))
+        * (
+            K.XX.offdiag_row_sums() @ K.XY.col_sums()
+            + K.YY.offdiag_row_sums() @ K.XY.row_sums()
         )
-        - ((4 * m - 6) / (mmm * m1_m1_m1))
-        * (K.XX.offdiag_sum() ** 2 + K.YY.offdiag_sum() ** 2)
-        + (4 * (m - 2) / (mmm * m1_m1))
-        * (K.XY.col_sums_sq_sum() + K.XY.row_sums_sq_sum())
-        - (4 * (m - 3) / (mmm * m1_m1)) * K.XY.sq_sum()
-        - ((8 * m - 12) / (mm * mmm * m1)) * K.XY.sum() ** 2
-        + (
-            (8 / (mmm * m1))
-            * (
-                1 / m * (K.XX.offdiag_sum() + K.YY.offdiag_sum()) * K.XY.sum()
-                - K.XX.offdiag_row_sums() @ K.XY.col_sums()
-                - K.YY.offdiag_row_sums() @ K.XY.row_sums()
-            )
-        )
+        + 8 / (mm * mdown3) * ((K.XX.offdiag_sum() + K.YY.offdiag_sum()) * K.XY.sum())
+        - (2 * twom3 / (mdown2 * mdown4)) * (K.XX.offdiag_sum() + K.YY.offdiag_sum())
+        - (4 * twom3 / (mmm * m1_m1_m1)) * K.XY.sum() ** 2
+        - (2 / (m * (mmm - 6 * mm + 11 * m - 6)))
+        * (K.XX.offdiag_sq_sum() + K.YY.offdiag_sq_sum())
+        + (4 * m2 / (mm * m1_m1_m1)) * K.XY.sq_sum()
     )
 
     return mmd_est, var_est
+
+
+def diff_mmd2_and_variance(kernel_pair_XY, kernel_pair_XZ, estimator=Estimator.U_STAT):
+    """
+    Core components of a test for whether MMD(X, Y) < MMD(X, Z),
+    assuming distributions of X, Y, Z are all distinct. Uses the U-statistic
+    estimator and its asymptotic variance.
+
+    Based on https://arxiv.org/abs/1511.04581v4, but with some slight bias
+    corrections: see notes/mmd-variance.pdf.
+    """
+
+    if estimator != Estimator.U_STAT:
+        warnings.warn(
+            "Computing asymptotic variance for U-statistic estimator, "
+            f"but using {estimator}."
+        )
+
+    if type(kernel_pair_XY) != type(kernel_pair_XZ):
+        warnings.warn(
+            f"XY is {type(kernel_pair_XY)} but XZ is {type(kernel_pair_XZ)}; "
+            "should be the same kernel...."
+        )
+        # TODO: check parameters also
+        # ...and check that X is the same?
+        # share _precompute_X between them?
+
+    kernel_pair_XZ = kernel_pair_XZ
+    assert (
+        kernel_pair_XY.n_X
+        == kernel_pair_XY.n_Y
+        == kernel_pair_XZ.n_X
+        == kernel_pair_XZ.n_Y
+    )
+
+    mmd2_XY = mmd2(kernel_pair_XY, estimator=estimator)
+    mmd2_XZ = mmd2(kernel_pair_XZ, estimator=estimator)
+    diff_est = mmd2_XY - mmd2_XZ
+
+    XY = kernel_pair_XY.XY
+    XZ = kernel_pair_XZ.XY
+    YY = kernel_pair_XY.YY
+    ZZ = kernel_pair_XZ.YY
+
+    m = XY.m
+    mm = m * m
+    mmm = mm * m
+    mmmm = mmm * m
+
+    m1 = m - 1
+    m1_m1 = m1 * m1
+    m1_m1_m1 = m1_m1 * m1
+
+    m2 = m - 2
+
+    mdown2 = m * m1
+    mdown3 = mdown2 * m2
+    mdown4 = mdown3 * (m - 3)
+
+    twom3 = 2 * m - 3
+
+    var_est = (
+        (4 * (mm - m - 1) / (mmm * m1_m1_m1))
+        * (
+            XY.row_sums_sq_sum()
+            + XY.col_sums_sq_sum()
+            + XZ.row_sums_sq_sum()
+            + XZ.col_sums_sq_sum()
+        )
+        + (4 / mdown4) * (YY.row_sums_sq_sum() + ZZ.row_sums_sq_sum())
+        - (8 / mmm * m1) * (XY.col_sums() @ XZ.col_sums())
+        - (8 / (mm * (mm - 3 * m + 2)))
+        * (
+            YY.offdiag_row_sums() @ XY.row_sums()
+            + ZZ.offdiag_row_sums() @ XZ.row_sums()
+        )
+        - (4 * twom3 / (mmm * m1_m1_m1)) * (XY.sum() ** 2 + XZ.sum() ** 2)
+        - (2 * twom3 / (mdown2 * mdown4))
+        * (YY.offdiag_sum() ** 2 + ZZ.offdiag_sum() ** 2)
+        + (8 / (mmmm * m1)) * (XY.sum() * XZ.sum())
+        + (8 / (mm * mdown3)) * (YY.sum() * XY.sum() + ZZ.sum() * XZ.sum())
+        - (4 * m2 / (mm * m1_m1_m1)) * (XY.sq_sum() + XZ.sq_sum())
+        - (2 / (m * (mmm - 6 * mm + 11 * m - 6)))
+        * (YY.offdiag_sq_sum() + ZZ.offdiag_sq_sum())
+    )
+
+    return diff_est, var_est
