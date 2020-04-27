6a63ea11a55e680538798ad6dc5a3f65bb41ce7b
D.J. Sutherland <djs@djsutherland.ml>
2020-04-27 13:08:26 -0500

    add a setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from pathlib import Path
+from setuptools import find_packages, setup
+
+here = Path(__file__).parent
+
+with open(here / "README.md") as f:
+    readme = f.read()
+with open(here / "requirements.txt") as f:
+    reqs = f.read()
+
+setup(
+    name="igms",
+    version="0.0.1",
+    description="Implementation of MMD and related implicit generative models for PyTorch.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dougalsutherland/igms",
+    author="DJ Sutherland",
+    author_email="mail@djsutherland.ml",
+    packages=find_packages(),
+    python_requires=">=3.6, <4",
+    install_requires=reqs,
+    classifiers=["License :: OSI Approved :: Apache Software License"],
+)
